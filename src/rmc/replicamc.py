74f72bc0d8c2cee271f0d892042fadd2cc1b98fe
Colin Clement <colin.clement@gmail.com>
2018-10-17 17:24:48 -0400

    replicamc cluster methods pass a few arbitrary tests

--- src/rmc/replicamc.py
+++ src/rmc/replicamc.py
@@ -2,98 +2,143 @@ import numpy as np
 from graph import Graph
 
 
-class SquareLattic(object):
-    def __init__(self, L):
+class SquareLattice(object):
+    def __init__(self, L, J):
         self.L = L
+        self.nv = L*L
+        self.ne = 2*L*L
+        self.J = J
+        assert J.shape == (2, L, L), "J is incorrect shape"
+        self._1100 = [1,1,0,0]
+
+    def flatidx(self, i, j):
+        return i*self.L + j
+    
+    def neighbors(self, i, flatidx=True):
+        assert 0<= i < self.nv, "index i out of range"
+        L = self.L
+        y, x = i//L, i%L
+        idxs = [[(y-1)%L, x], [(y+1)%L, x], [y, (x-1)%L], [y, (x+1)%L]]
+        return [self.flatidx(*idx) for idx in idxs] if flatidx else idxs
+
+    def neighbonds(self, i, flatidx=True):
+        L = self.L
+        n = self.neighbors(i, flatidx)
+        return n, [self.J[d, nn//L, nn%L] for d, nn in zip(self._1100, n)]
+
+    def bond(self, i, j):
+        for n, J in zip(*self.neighbonds(i)):
+            if i == n:
+                return J
+
+
+class GeneralLattice(Graph):
+    def __init__(self, nvertices):
+        super().__init__(nvertices)
 
     def neighbors(self, i):
-        y, x = i//self.L, i%self.L
-        return 
+        edge = self.edges[i]
+        idxs = []
+        while edge:
+            idxs.append(edge.y)
+            edge = edge.nextedge
+        return idxs
+
+    def neighbonds(self, i):
+        edge = self.edges[i]
+        idxs, bonds = [], []
+        while edge:
+            idxs.append(edge.y)
+            bonds.append(edge.w)
+            edge = edge.nextedge
+        return idxs, bonds
+
+    def bond(self, i, j):
+        edge = self.edges[i]
+        while edge:
+            if edge.y == j:
+                return edge.w
+            edge = edge.nextedge
+
 
 class ReplicaMonteCarlo(object):
     def __init__(self, L, tlist=[0.1, 1.], **kwargs):
         self.L = L
         self.tlist = np.array(tlist)
         self.rng = np.random.RandomState(kwargs.get('seed', 14850))
-        self.s = kwargs.get("s", 2*(self.rng.rand(len(tlist), L,L)>0.5) - 1)
+        self.s = kwargs.get("s", 2*(self.rng.rand(len(tlist), L*L)>0.5) - 1)
         self.J = kwargs.get("J", self.rng.randn(2, L, L))
-
-    def neighbors(self, i, j, flatidx=False):
-        L = self.L
-        idxs = [[(i-1)%L, j], [(i+1)%L, j], [i, (j-1)%L], [i, (j+1)%L]]
-        return [idx[0]*L+idx[1] for idx in idxs] if flatidx else idxs
-
-    def flatidx(self, i, j):
-        return i*self.L + j
-
-    def neighbonds(self, i, j, flatidx=False):
-        n = self.neighbors(i,j, flatidx)
-        return n, [[d, n[0], n[1]] for d, n in zip([1,1,0,0], n) ]
-
-    def flipenergy(self, s, i, j):
-        ns, bs = self.neighbonds(i, j)
-        return -2*s[i,j]*sum([b*s[n] for n, b in zip(ns, bs)])
+        self.G = SquareLattice(self.L, self.J)
+        self.t = 0
 
     @property
     def blist(self):
         return 1./tlist
 
-    def singlespinsweep(self, s, b):
-        for i in range(self.L):
-            for j in range(self.L):
-                dE = self.flipenergy(s, i, j)
-                if dE < 0 or np.exp(-b*dE) > self.rng.randn():
-                    s[i,j] = -s[i,j]
+    def deltaE(self, i, s, G):
+        ns, bs = self.G.neighbonds(i, j)
+        return 2*s[i]*sum([b*s[n] for n, b in zip(ns, bs)])
+
+    def singlespinsweep(self, s, G, b):
+        for i in range(len(s)):
+            if dE < 0 or np.exp(-b*self.deltaE(i, s, G)) > self.rng.randn():
+                s[i] = -s[i]
 
     def mhsweep(self):
         for s, b in zip(self.s, self.blist):
-            self.singlespinsweep(s, b)
+            self.singlespinsweep(s, self.G, b)
 
-    def swclust(self, s1, s2):
-        L = self.L
-        tau = np.ravel(s1*s2)
-        spins = set(np.arange(tau.size))
-        clusters, outbounds = [], []
-        while spins:
-            seed = spins.pop()
-            clust = {seed}
-            queue = [seed]
-            outb ={} 
-            while queue:
-                nxt = queue.pop()
-                neighs = self.neighbors(nxt//L, nxt%L, True)
-                for n in neighs:
-                    if not tau[nxt] == tau[n]: 
-                        neighs.remove(n)
-                        outb.update(n)
-                clust.update(neighs)
-                queue.extend(neighs)
-                spins.difference_update(neighs)
-            outbounds.append(outb)
-            clusters.append(clust)
-        return clusters, outbounds
+    def swsweep(self):
+        s, b = self.s, self.blist
+        for i in range(len(b)-1):
+            s1, s2 = s[i], s[i+1]
+            clusterG = clustergraph(s1, s2, self.G)
+            tau = s1 * s2
+            self.singlespinsweep(tau, clusterG, b[i]-b[i+1])
+            s1 *= tau
+            s2 *= tau
 
-    def clustergraph(self, s1, s2, b1, b2):
-        clusters, outbounds = self.swclust(s1, s2)
-        L = self.L
-        G = Graph(len(clusters))
-        for i, (c0, ob0) in enumerate(zip(clusters, outbounds)):
-            for j, (c1, ob1) in enumerate(zip(clusters[i:], outbounds[1:])):
-                bond = 0
-                for idx in ob0:
-                    idx_i, idx_j = idx//L, idx%L
-                    for n in self.neighbors(idx_i, idx_j, True):
-                        if n in ob1:
-                            n_i, n_j = n//L, n%L
-                            gauge = s1[idx_i, idx_j]*s1[n_i, n_j]
-                            if n == idx+1:  # right
-                                bond += self.J[0, idx_i, idx_j]*gauge
-                            elif n == idx-1:  # left
-                                bond += self.J[0, n_i, n_j]*gauge
-                            elif n == idx+L:  # down
-                                bond += self.J[1, idx_i, idx_j]*gauge
-                            elif n == idx-L:  # up
-                                bond += self.J[1, n_i, n_j]*gauge
-                if not bond == 0:
-                    G.insert(i, j, w=(b1 - b1)*bond)
-        return G
+    def step(self):
+        self.mhsweep()
+        self.swsweep()
+        self.t += 1
+
+
+def swendsenwangcluster(s1, s2, G):
+    L = s1.shape[0]
+    tau = s1*s2
+    spins = set(np.arange(tau.size))
+    clusters, outbounds = [], []
+    while spins:
+        seed = spins.pop()
+        clust = {seed}
+        queue = [seed]
+        outb =set([])
+        while queue:
+            neighs = set(G.neighbors(queue.pop(), True))
+            neighs = neighs.difference(clust)
+            for n in list(neighs):
+                if not tau[seed] == tau[n]: 
+                    neighs.remove(n)
+                    outb.add(n)
+            clust.update(neighs)
+            queue.extend(neighs)
+            spins.difference_update(neighs)
+        outbounds.append(outb)
+        clusters.append(clust)
+    return clusters, outbounds
+
+def clustergraph(s1, s2, squareG):
+    clusters, outbounds = swendsenwangcluster(s1, s2, squareG)
+    L, J = squareG.L, squareG.J
+    G = GeneralLattice(len(clusters))
+    for i, (c0, ob0) in enumerate(zip(clusters, outbounds)):
+        for j, (c1, ob1) in enumerate(zip(clusters[i+1:], outbounds[i+1:])):
+            clusterbond = 0
+            for idx in ob0.intersection(c1):
+                for n, bond in zip(*squareG.neighbonds(idx)):
+                    if n in c0:
+                        clusterbond += bond * s1[idx] * s1[n]
+            if not bond == 0:
+                G.insert(i, j+i+1, w=clusterbond)
+    return G
--- src/rmc/test.py
+++ src/rmc/test.py
@@ -2,8 +2,39 @@
 Tests to make sure things work
 """
 
+import numpy as np
+from itertools import repeat, product
+import scipy.ndimage as ndi
+import replicamc
+
+rng = np.random.RandomState(14850)
+
+def hamiltonian(J, s):
+    s = np.array(list(s)).reshape(J.shape[1:])
+    sh = s * np.roll(s, -1)
+    sv = s * np.roll(s, -1, 0)
+    return -np.sum(J[0]*sh + J[1]*sv)
+
 def smallprobabilitytest():
     """ 
     for a 4x4 system exactly enumerate states and compare to rmc
     """
-    pass
+    L = 4
+    J = rng.randn(2, L, L)
+    tlist = np.array([.1, 1.])
+    blist = 1./tlist
+    #mc = replicamc.ReplicaMonteCarlo(L, J=J)
+
+    allstates = product([1,-1], repeat=L*L)
+    energies = list(map(hamiltonian, repeat(J), allstates))
+    probs = np.array([np.exp(-b*np.array(energies)) for b in blist])
+    probs /= probs.sum(1)[:,None]
+    return probs
+
+def testcluster(p=0.3):
+    img = 1*(np.random.rand(100,100)<p)
+    img = ndi.binary_closing(img, border_value=1)
+    lab, num = ndi.label(img)
+    ind = np.arange(img.size+1)
+    clusters = [set(ind[lab==i]) for i in range(num)]
+
