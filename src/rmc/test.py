3bd13de60c5cc392c72b8463481a969d8c503d86
Colin Clement <colin.clement@gmail.com>
2018-10-19 16:02:39 -0400

    probability test written

--- src/rmc/test.py
+++ src/rmc/test.py
@@ -9,21 +9,33 @@ from collections import Counter
 import replicamc
 
 rng = np.random.RandomState(14850)
+L = 4
+J = rng.randn(2, L, L)
+blist=[1.5, 1.]
 
-def smallprobabilitytest():
+def allstates(L=4):
+    return product([1,-1], repeat=L*L)
+
+def exactprobs(J, blist):
     """ 
     for a 4x4 system exactly enumerate states and compare to rmc
     """
-    L = 4
-    J = rng.randn(2, L, L)
-    tlist = np.array([.1, 1.])
-    blist = 1./tlist
-    #mc = replicamc.ReplicaMonteCarlo(L, J=J)
+    L = J.shape[1]
 
-    allstates = product([1,-1], repeat=L*L)
-    energies = list(map(hamiltonian, repeat(J), allstates))
+    G = replicamc.SquareLattice(L, J)
+    energies = list(map(G.hamiltonian, allstates()))
     probs = np.array([np.exp(-b*np.array(energies)) for b in blist])
     probs /= probs.sum(1)[:,None]
     return probs
 
+def countstates(J, blist, n=200000):
+    L = J.shape[1]
+    mc = replicamc.ReplicaMonteCarlo(J.shape[2], J=J, tlist=1./np.array(blist))
+    samples = np.array(mc.run(n, period=2, thin=20))
+    counters = [Counter(map(tuple, samples[:,i])) for i in
+                range(samples.shape[1])]
+    N = len(samples)
+    probs = [[cnt[tuple(s)]/N for s in allstates()] 
+            for i, cnt in enumerate(counters)]
+    return np.array(probs)
 
