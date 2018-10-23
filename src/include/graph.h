788804cb2347d6e82206326e45f83e06e3d39425
Colin Clement <colin.clement@gmail.com>
2018-10-23 13:28:43 -0400

    added in correct mod and neighbor square lattice macros

--- src/include/graph.h
+++ src/include/graph.h
@@ -1,6 +1,15 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+// how we can define the square lattice
+#define MOD(x, N) (((x < 0) ? ((x % N) + N) : x) % N)
+#define idx(y, x, L) (y*L + x)
+#define neighbors(y, x, L) {idx(y, MOD(x-1, L), L),\
+                            idx(y, MOD(x+1, L), L),\
+                            idx(MOD(y-1,L), x, L),\
+                            idx(MOD(y+1,L), x, L)}
+#define coordination 4
+
 typedef struct edgenode edgenode;
 
 // structure which points between vertices of a graph
