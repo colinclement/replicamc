7a624462ebcfad69dc7061e88d1955ed204c0a7f
Colin Clement <colin.clement@gmail.com>
2018-10-23 13:29:47 -0400

    documentation

--- src/include/cluster.h
+++ src/include/cluster.h
@@ -1,6 +1,12 @@
 #ifndef CLUSTER_H
 #define CLUSTER_H
 
+// given two spin configurations s1 and s2 of size L*L, this function finds the 
+// clusters of same value of tau[i]=s1[i]*s2[i] by filling the list of lists
+// clusters with linked lists of cluster indices. outbounds is filled with lists 
+// containing the spins which are adjacent to each cluster but not contained in
+// it. These indices will allow us to form the adjacency graph upon which
+// Metropolis-Hastings steps can be taken.
 void swendsenwangcluster(int *s1, int *s2, lol **clusters, lol **outbounds, int L);
 
 #endif
