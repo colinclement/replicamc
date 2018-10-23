327a4e431897f9dfac94d3a98e250c5afd851ba3
Colin Clement <colin.clement@gmail.com>
2018-10-23 13:29:34 -0400

    tested swendsenwangcluster and passed memory leak check

--- src/src/cluster.c
+++ src/src/cluster.c
@@ -1,17 +1,16 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <graph.h>
 #include <ll.h>
 
-#define idx(y, x, L) (y*L + x)
-#define neighbors(y, x, L) {idx(y, ((x-1)%L), L),\
-                            idx(y, ((x+1)%L), L),\
-                            idx(((y-1)%L), x, L),\
-                            idx(((y+1)%L), x, L)}
-#define coordination 4
 
 void swendsenwangcluster(int *s1, int *s2, lol **clusters, lol **outbounds, int L) {
     int N = L*L;
     int *tau = (int *)malloc(N*sizeof(int));
+    int *touched = (int *)malloc(N*sizeof(int));
+    checkptr(tau)
+    checkptr(touched)
     ll *spins = initList(0);
 
     for (int i=0; i < N; i++) {
@@ -21,37 +20,50 @@ void swendsenwangcluster(int *s1, int *s2, lol **clusters, lol **outbounds, int 
     }
 
     int Nc = 0;
+    // iterate over all the spins
     while (spins != NULL) {
+        memset(touched, 0, N*sizeof(int));
+
         int seed = popint(&spins);
+        touched[seed] = 1;
         ll *clust = initList(seed);
         ll *queue = initList(seed);
-        ll *outb = initList(seed);
+        ll *outb = initList(-1);
 
+        // try making a new cluster
         while (queue != NULL) {
             int next = popint(&queue);
-            int neighs[] = neighbors(next/L, next%L, L);
+            int neighs[coordination] = neighbors(next/L, next%L, L);
+
             for (int i=0; i < coordination; i++) {
                 int n = neighs[i];
-                if (search(n, clust))  // ignore sites in clust
+                if (touched[n])  // ignore sites in clust
                     continue;
                 else if (tau[seed] == tau[n]) {
+                    touched[n] = 1;
                     pushint(n, &clust);
                     pushint(n, &queue);
                     removeval(n, &spins);
                 } else {
+                    touched[n] = 1;
                     pushint(n, &outb);
                 }
             }
         }
+        destroyList(queue);
+        removeval(-1, &outb);
 
         if (Nc == 0) {  // initialized clusters have trivial lists
-            (*clusters)->list = clust;
-            (*outbounds)->list = outb;
+            *clusters = initListofLists(clust);
+            *outbounds = initListofLists(outb);
         } else {
             pushList(clust, clusters);
             pushList(outb, outbounds);
         }   
         Nc += 1;
     }
+
+    destroyList(spins);
     free(tau);
+    free(touched);
 }
--- src/src/test.c
+++ src/src/test.c
@@ -2,51 +2,67 @@
 #include <stdio.h>
 #include <graph.h>
 #include <ll.h>
+#include <cluster.h>
 
 int main(int argc, const char* argv[])
 {
-    graph *G = initGraph(3);    
-    insertEdge(0, 1, 1, G);
-    insertEdge(1, 2, 1, G);
-    insertEdge(2, 1, 1, G);
-    printGraph(G);
-    int s[3] = {1, -1, -1};
-    printf("%f\n", deltaE(0, s, G));
-    destroyGraph(G);
+    //graph *G = initGraph(3);    
+    //insertEdge(0, 1, 1, G);
+    //insertEdge(1, 2, 1, G);
+    //insertEdge(2, 1, 1, G);
+    //printGraph(G);
+    //int s[3] = {1, -1, -1};
+    //printf("%f\n", deltaE(0, s, G));
+    //destroyGraph(G);
 
-    ll *list = initList(0);
-    pushint(1, &list);
-    pushint(2141, &list);
-    pushint(4, &list);
-    printll(list);
-    printf("is 0 in the list? %i\n", search(0, list));
-    printf("is 1 in the list? %i\n", search(1, list));
-    printf("is 5 in the list? %i\n", search(5, list));
-    printf("is 2141 in the list? %i\n", search(2141, list));
-    ll *list2 = initList(102);
+    //ll *list = initList(0);
+    //pushint(1, &list);
+    //pushint(2141, &list);
+    //pushint(4, &list);
+    //printll(list);
+    //printf("is 0 in the list? %i\n", search(0, list));
+    //printf("is 1 in the list? %i\n", search(1, list));
+    //printf("is 5 in the list? %i\n", search(5, list));
+    //printf("is 2141 in the list? %i\n", search(2141, list));
+    //ll *list2 = initList(102);
 
-    pushint(1230, &list2);
+    //pushint(1230, &list2);
 
-    lol *lolists = initListofLists(list);
-    pushList(list2, &lolists);
-    printlol(lolists);
+    //lol *lolists = initListofLists(list);
+    //pushList(list2, &lolists);
+    //printlol(lolists);
 
-    ll *list3 = initList(102);
-    pushint(1230, &list3);
-    pushint(10, &list3);
-    //int popped = popint(&list3);
-    //printf("popped %i\n", popped);
-    printf("printing list3\n");
-    printll(list3);
-    //popint(&(list3->next->next));
-    removeval(10, &list3);
-    removeval(1230, &list3);
-    removeval(102, &list3);
-    printf("list3 is null %i\n", (list3==NULL));
-    printf("printing list3\n");
-    printll(list3);
+    //ll *list3 = initList(102);
+    //pushint(1230, &list3);
+    //pushint(10, &list3);
+    ////int popped = popint(&list3);
+    ////printf("popped %i\n", popped);
+    //printf("printing list3\n");
+    //printll(list3);
+    ////popint(&(list3->next->next));
+    //removeval(10, &list3);
+    //removeval(1230, &list3);
+    //removeval(102, &list3);
+    //printf("list3 is null %i\n", (list3==NULL));
+    //printf("printing list3\n");
+    //printll(list3);
 
-    destroyListofLists(lolists);
-    destroyList(list3);
+    //destroyListofLists(lolists);
+    //destroyList(list3);
+    int L=4;
+    int s1[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    int s2[] = {1,1,1,1,1,1,-1,1,1,-1,-1,1,1,1,1,1};
+
+    lol *clusters, *outbounds;
+
+    swendsenwangcluster(s1, s2, &clusters, &outbounds, L);
+    printf("clusters:\n");
+    printlol(clusters);
+    printf("outbounds:\n");
+    printlol(outbounds);
+
+    destroyListofLists(clusters);
+    destroyListofLists(outbounds);
+
     return 0;
 }
