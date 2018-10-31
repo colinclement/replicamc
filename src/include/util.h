9f9d86618e9105858e3f3b89e59d67bd3525d5c9
Colin Clement <colin.clement@gmail.com>
2018-10-31 00:12:18 -0400

    added file reader for init

--- src/include/util.h
+++ src/include/util.h
@@ -1,6 +1,19 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+typedef struct init init;
+
+struct init {
+    int L;
+    int t[3]; //burntime, run time, save period
+    int Nb;
+    float *b;  // Nb betas
+};
+
+void readInputFile(const char *fname, init *initvalues);
+
+void destroyInit(init *initvalues);
+
 float pcgrand(pcg32_random_t *rng);
 
 float pcgrandn(pcg32_random_t *rng);
--- src/src/util.c
+++ src/src/util.c
@@ -1,15 +1,69 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
+#include <stdio.h>
 #include <math.h>
 #include <pcg_basic.h>
+#include <util.h>
 
 #define RAND32_MAX 4294967295 // 2**32 - 1
 
+void readInputFile(const char *fname, init *initvalues){
+    /* reads file fname and populates init struct
+     *
+     * expected file structure:
+     * L
+     * burntime runtime savetime
+     * Nb
+     * b0
+     * b1
+     * b2
+     * ...
+     *  b_(Nb-1)
+     */
+    FILE *fp = fopen(fname, "r");
+    if (NULL == fp){
+        fprintf(stderr, "Unable to open file %s", fname);
+        exit(1);
+    }
+
+    fscanf(fp, "%i", &(initvalues->L));
+    fscanf(fp, "%i %i %i", initvalues->t, initvalues->t+1, initvalues->t+2);
+    fscanf(fp, "%i", &(initvalues->Nb));
+
+    initvalues->b = (float *)malloc(initvalues->Nb * sizeof(float));
+    int i=0;
+    while (fscanf(fp, "%f", initvalues->b + i) != EOF){
+        i++;
+        //fgetc(fp);  // moves past newline
+        if (i > initvalues->Nb){
+             fprintf(stderr, "Found %i betas, expected %i\n", i, 
+                    initvalues->Nb);
+             exit(1);
+        }
+    }
+    if (i < initvalues->Nb){
+            fprintf(stderr, "Found %i betas, expected %i\n", i, 
+                    initvalues->Nb);
+            exit(1);
+        }
+     
+    printf("%i\n", i);
+    fclose(fp);
+}
+
+void destroyInit(init *initvalues){
+    free(initvalues->b);
+}
+
 float pcgrand(pcg32_random_t *rng){
+    assert(NULL != rng);
     return pcg32_random_r(rng) * (1.0 / RAND32_MAX);
 }
 
 float pcgrandn(pcg32_random_t *rng){
+    assert(NULL != rng);
+
     float U1, U2, W=0, mult;
     static float X1, X2;  // save these values between calls!
     static int call = 0;
@@ -33,6 +87,9 @@ float pcgrandn(pcg32_random_t *rng){
 }
 
 void printSpins(int *s, int L) {
+    assert(NULL != s);
+    assert(L > 0);
+
     for (int i=0; i < L; i++){
         for (int j=0; j < L; j++){
             printf("%i ", s[i*L + j]);
