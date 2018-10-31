4f5631eb3ed1b960761d6a857a508f9d1a4ae716
Colin Clement <colin.clement@gmail.com>
2018-10-31 00:14:33 -0400

    tested init reader

--- src/src/test.c
+++ src/src/test.c
@@ -10,6 +10,19 @@
 
 int main(int argc, const char* argv[])
 {
+    if (argc != 2){
+        printf("Usage: test input.txt\n");
+        return 0;
+    } else {
+        init initvalues;
+        readInputFile(argv[1], &initvalues);
+        printf("L=%i\n", initvalues.L);
+        printf("t=%i %i %i\n", initvalues.t[0], initvalues.t[1],
+               initvalues.t[2]);
+        printf("Nb=%i\n", initvalues.Nb);
+        for (int i=0; i < initvalues.Nb; i++)
+            printf("%f\n", initvalues.b[i]);
+    }
 
     pcg32_random_t rng;
     pcg32_srandom_r(&rng, 42u, 54u); // seed deterministically
@@ -18,19 +31,22 @@ int main(int argc, const char* argv[])
     int Nb=2;
     float b[] = {1.5, 1.};
     int *s = (int *)malloc(Nb*L*L*sizeof(int));
+    int *label = (int *)malloc(L*L*sizeof(int));
     randomize(s, Nb, L, &rng);
 
+    // fill random bonds
     float *J = (float *)malloc(2*L*L*sizeof(float));
     for (int i=0; i < 2*L*L; i++)
         J[i] = pcgrandn(&rng);
 
     for (int t=0; t < 1000; t++){
         mhsweep(s, b, Nb, J, L, &rng);
-        swstep(s, b, Nb, J, L, &rng);
+        swstep(s, b, Nb, J, label, L, &rng);
     }
 
     free(s);
     free(J);
+    free(label);
 
     return 0;
 }
