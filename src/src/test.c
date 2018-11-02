f95507491dfbbce8e62e53419590187d16cb03e5
Colin Clement <colin.clement@gmail.com>
2018-11-02 16:41:25 -0400

    random seeds and argc with seeds, save and burnin, memory leak-check passed

--- src/src/test.c
+++ src/src/test.c
@@ -1,35 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#include <pcg_basic.h>
+
 #include <graph.h>
 #include <cluster.h>
-#include <pcg_basic.h>
 #include <mc.h>
 #include <ll.h>
 #include <util.h>
 
+#define MAXLEN 256
 
 int main(int argc, const char* argv[])
 {
-    if (argc != 2){
-        printf("Usage: test input.txt\n");
-        return 0;
-    } else {
-        init initvalues;
-        readInputFile(argv[1], &initvalues);
-        printf("L=%i\n", initvalues.L);
-        printf("t=%i %i %i\n", initvalues.t[0], initvalues.t[1],
-               initvalues.t[2]);
-        printf("Nb=%i\n", initvalues.Nb);
-        for (int i=0; i < initvalues.Nb; i++)
-            printf("%f\n", initvalues.b[i]);
+    init initvalues;
+    
+    // Random initial seeds set by time and memory address
+    unsigned int s0=time(NULL) ^ (intptr_t)&printf, s1=(intptr_t)&initvalues;
+
+    switch (argc) {
+        case 2 :
+            readInputFile(argv[1], &initvalues);
+            break;
+        case 4 :
+            readInputFile(argv[1], &initvalues);
+            s0 = strtoul(argv[2], NULL, 0);
+            s1 = strtoul(argv[2], NULL, 0);
+            break;
+        default:
+            printf("Requires 1 or 3 arguments.\n Usage: test inputfile "
+                   "[seed0 seed1]\n");
+            return 0;
     }
 
-    pcg32_random_t rng;
-    pcg32_srandom_r(&rng, 42u, 54u); // seed deterministically
+    char outname[MAXLEN];
+    makeOutputName(argv[1], outname, NULL);
+    char bondname[MAXLEN];
+    makeOutputName(argv[1], bondname, "-bonds");
 
-    int L=4;
-    int Nb=2;
-    float b[] = {1.5, 1.};
+    pcg32_random_t rng;
+    pcg32_srandom_r(&rng, s0, s1); // seed deterministically
+
+    int L=initvalues.L;
+    int Nb=initvalues.Nb;
+
+    float *b = initvalues.b;
     int *s = (int *)malloc(Nb*L*L*sizeof(int));
     int *label = (int *)malloc(L*L*sizeof(int));
     randomize(s, Nb, L, &rng);
@@ -39,14 +56,27 @@ int main(int argc, const char* argv[])
     for (int i=0; i < 2*L*L; i++)
         J[i] = pcgrandn(&rng);
 
-    for (int t=0; t < 1000; t++){
+    saveBonds(bondname, J, L);
+
+    // Burn in (throw away sequences)
+    for (int t=0; t < initvalues.t[0]; t++){
         mhsweep(s, b, Nb, J, L, &rng);
         swstep(s, b, Nb, J, label, L, &rng);
     }
 
+    FILE *fp = fopen(outname, "a");
+    for (int t=0; t < initvalues.t[1]; t++){
+        mhsweep(s, b, Nb, J, L, &rng);
+        swstep(s, b, Nb, J, label, L, &rng);
+        if (t % initvalues.t[2] == 0)
+            writeSpins(fp, s, L, Nb);
+    }
+
+    fclose(fp);
     free(s);
     free(J);
     free(label);
+    destroyInit(&initvalues);
 
     return 0;
 }
